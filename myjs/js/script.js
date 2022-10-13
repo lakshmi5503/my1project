5a9e58f1ab3b1e923f7f23f37c9167856915c3bc
subbu10136 <33937989+subbu10136@users.noreply.github.com>
2022-10-13 23:05:27 +0530

    testing

---
+++ myjs/js/script.js
@@ -0,0 +1,10 @@
+function mybulma(){
+    var mydiv=document.getElementById("one")
+    mydiv.style.display="none"
+    }
+
+    
+    function mybulma1(){
+    var mydiv=document.getElementById("one")
+    mydiv.style.display="block"
+    }
